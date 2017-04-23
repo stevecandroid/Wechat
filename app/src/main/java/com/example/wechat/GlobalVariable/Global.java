2e994e0927ff8171cf7852cbcecc726b6b4095d0
c <214652773@qq.com>
2017-04-23 12:28:18 +0800

    FuckingStarBoy

--- app/src/main/java/com/example/wechat/GlobalVariable/Global.java
+++ app/src/main/java/com/example/wechat/GlobalVariable/Global.java
@@ -5,6 +5,8 @@ import android.app.Application;
 import android.content.Context;
 import android.util.Log;
 
+import com.example.wechat.Uitls.ActivityHelper;
+
 /**
  * Created by 铖哥 on 2017/4/23.
  */
@@ -12,17 +14,18 @@ import android.util.Log;
 public class Global extends Application {
 
     private static Context mContext;
-    private static Activity mCurrentActivity;
+
 
     public static Activity getmCurrentActivity() {
         //TODO 加入得到栈顶活动的逻辑
-        return mCurrentActivity;
-    }
 
-    public static void setmCurrentActivity(Activity mCurrentActivity) {
-        //TODO 加入得到栈顶活动的逻辑
-        Global.mCurrentActivity = mCurrentActivity;
+        return  ActivityHelper.getTopActivity();
     }
+//
+//    public static void setmCurrentActivity(Activity mCurrentActivity) {
+//        //TODO 加入得到栈顶活动的逻辑
+//        Global.mCurrentActivity = mCurrentActivity;
+//    }
 
     public static Context getContext(){
         return mContext;
