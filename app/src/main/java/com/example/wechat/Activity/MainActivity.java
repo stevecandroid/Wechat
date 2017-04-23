7a4e1db482a08110a89785296e5f0ef3c22dd3e3
jim ji <1035363309@qq.com>
2017-04-23 10:50:10 +0800

    加toastHelper

--- app/src/main/java/com/example/wechat/Activity/MainActivity.java
+++ app/src/main/java/com/example/wechat/Activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.wechat;
+package com.example.wechat.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.wechat.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/example/wechat/Uitls/ToastHelper.java
@@ -0,0 +1,22 @@
+package com.example.wechat.Uitls;
+
+import android.widget.Toast;
+
+/**
+ * 项目名： Wechat
+ * 创建人： 小吉哥哥
+ * 创建人： 2017/4/23.
+ */
+
+public class ToastHelper {
+
+    private static Toast mToast;
+    private static Object mObject;
+    public static void Toast(Object o){
+        if(null == mToast || !o.toString().equals(mObject.toString())){
+            if(null == mToast){
+
+            }
+        }
+    }
+}
