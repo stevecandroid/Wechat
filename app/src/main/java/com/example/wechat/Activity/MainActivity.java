e0f24368dfd141c889186c51e12d3c8eb8dba681
jim ji <1035363309@qq.com>
2017-04-23 12:12:59 +0800

    加入bmob

--- app/src/main/java/com/example/wechat/Activity/MainActivity.java
+++ app/src/main/java/com/example/wechat/Activity/MainActivity.java
@@ -6,35 +6,19 @@ import android.webkit.WebView;
 
 import com.example.wechat.GlobalVariable.Global;
 import com.example.wechat.R;
+import com.example.wechat.Uitls.LogHelper;
 import com.example.wechat.Uitls.ToastHelper;
 
 public class MainActivity extends AppCompatActivity {
 
-    WebView wb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Global.setmCurrentActivity(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for(int i = 0;i<10;i++){
-                    ToastHelper.Toast(i);
-                    try {
-                        Thread.sleep(500);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }).start();
+        LogHelper.Log(1);
 
-
-
-
-        
 
     }
 }
